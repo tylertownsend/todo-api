f1ac55c0eaebcf6f7601cd4d778305bc25023aba
Tyler S. Townsend <ttown@knights.ucf.edu>
2019-03-14 23:42:05 -0400

    updated authentication for patch, delete, and tests for logging out

--- tests/user.test.js
+++ tests/user.test.js
@@ -21,8 +21,7 @@ beforeEach(async () => {
 });
 describe('POST /users', () => {
   it('Should signup a new user', async () => {
-    await request(app)
-      .post('/users')
+    await request(app) .post('/users')
       .send({name: 'Tyler',
             email: 'tyler@example.com',
             password: 'MyPass777!'
@@ -30,40 +29,51 @@ describe('POST /users', () => {
   });
 
   it('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
-      email: userOne.email,
-      password: userOne.password
-    }).expect(200);
+    await request(app).post('/users/login')
+      .send({
+        email: userOne.email,
+        password: userOne.password
+      }).expect(200);
   });
 
-  it('Should not login nonexistent user', async() =>{
-    await request(app).post('/users').send({
-      email: 'tylernotfound@example.com',
-      password: userOne.password
-    }).expect(400);
+  it('Should not login nonexistent user', async() => {
+    await request(app).post('/users')
+      .send({
+        email: 'tylernotfound@example.com',
+        password: userOne.password
+      }).expect(400);
   });
 
-  it('Should reject the new email', async ()=> {
-    await request(app).post('/users').send({
-      email: 'Now a valid email',
-      password: userOne.password
-    }).expect(400);
+  it('Should reject the new email', async () => {
+    await request(app).post('/users')
+      .send({
+        email: 'Now a valid email',
+        password: userOne.password
+      }).expect(400);
   });
 
-  it('Should reject the password', async ()=> {
-    await request(app).post('/users').send({
-      email: userOne.email,
-      password: '1'
-    }).expect(400);
+  it('Should reject the password', async () => {
+    await request(app).post('/users')
+      .send({
+        email: userOne.email,
+        password: '1'
+      }).expect(400);
+  });
+
+  it('Should logout the user', async() => {
+    await request(app).post('/users/logout')
+      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+      .expect(200);
+  });
+
+  it('Should not logout the user when unauthorized', async() => {
+    await request(app).post('/users/logout')
+      .expect(401);
   });
 });
 
 describe('GET /users', () => {
   it('Should get profile for user', async ()=> {
-    // await request(app).post('/users/login').send({
-    //   email: userOne.email,
-    //   password: userOne.password
-    // }).expect(200);
     await request(app).get('/users/me')
       .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
       .send()
@@ -74,5 +84,53 @@ describe('GET /users', () => {
     await request(app).get('/users/me')
       .send()
       .expect(401);
+  });
+});
+
+describe('DELETE /users', () => {
+  it('Should delete account for user', async ()=> {
+    await request(app).delete('/users/me')
+      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+      .send()
+      .expect(200);
+  });
+
+  it('Should not delete account for unauthenticated user', async () => {
+    await request(app).delete('/users/me')
+      .send()
+      .expect(401);
+  });
+});
+
+describe('PATCH /users', ()=> {
+  it('Should update user account information', async () => {
+    await request(app).patch('/users/me')
+      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+      .send({
+        'name': 'Elon',
+        'email': 'spacex@mail.com',
+        'password': 'TelsaBaby!!'
+      })
+      .expect(200);
+  });
+
+  it('Should not update an user when unauthorized', async () => {
+    await request(app).patch('/users/me')
+      .send({
+        'name': 'Elon',
+        'email': 'spacex@mail.com',
+        'password': 'TelsaBaby!!'
+      })
+      .expect(401);
+  });
+
+  it('Should not update an invalid parameter', async () => {
+    await request(app).patch('/users/me')
+      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+      .send({
+        'favorite color': 'blue',
+      })
+      .expect(400);
+
   });
 });

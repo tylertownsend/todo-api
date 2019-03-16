bcba6df8743556e44d0690fcad39c6e14cbe5387
Tyler S. Townsend <ttown@knights.ucf.edu>
2019-03-16 17:48:52 -0400

    updated get /tasks to accept queries for pagination and sorting

--- src/routers/tasksRouter.js
+++ src/routers/tasksRouter.js
@@ -18,9 +18,30 @@ taskRouter.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// Get/tasks?completed=true
+// Get /tasks?limit=10&skip=20
 taskRouter.get('/tasks', auth, async (req, res)=> {
+  const match = {}
+  const sort = {}
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort: (req.query.sortBy)
+      }
+    }).execPopulate();
     res.send(req.user.tasks);
   } catch(e) {
     res.status(500).send(e);
